10c6af9f9a6c6f3bd027477488b6fc847464d8ff
dioic3 <joyce.dias@gec.inatel.br>
2024-01-09 14:19:37 -0300

    adicionando outras anotações

--- aula6/index.js
+++ aula6/index.js
@@ -2,3 +2,6 @@ const primeiroNumero = 5;
 const segundoNumero = 10;
 const conta = primeiroNumero * segundoNumero;
 console.log(conta)
+//usar o typeof para descobrir a tipagem da variavel
+console.log(typeof conta)
+// a soma no js serve para juntar string e numero, e somar numeros.
