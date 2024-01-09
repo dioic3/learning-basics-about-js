c47c627d5e0ceaf9ee74ca86998e006a3d67ade9
dioic3 <joyce.dias@gec.inatel.br>
2024-01-09 14:09:07 -0300

    adição da aula 4 até aula 6

---
+++ aula5/index.js
@@ -0,0 +1,13 @@
+let nome = 'João';
+console.log(nome, 'nasceu em 1984.')
+
+let big;
+big = 'Qualquer valor';
+console.log(big);
+//Não podemos criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+//Não podemos começar o nome de uma variável com um número
+//Não conter espaços ou traços
+//Utilizar o conceito de camelCase
+//É case-sensitive
+//Não podemos redeclarar variáveis com let
---
+++ aula6/index.js
@@ -0,0 +1,4 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const conta = primeiroNumero * segundoNumero;
+console.log(conta)
